0a80eee6e6889b9ee495f17661e9b2f355b92b9c
Jan Mareš <marosh.dev@gmail.com>
2024-04-29 11:35:25 +0200

    feat: Initialize user application structure and add max7219 library
    - Create new Application folder for user app
    - Add empty app.c and app.h files
    - Add max7219 library for 7 segment display

---
+++ STM32G0_USBPD_Sink_1.2/Application/app.c
@@ -0,0 +1,8 @@
+/*
+ * app.c
+ *
+ *  Created on: Apr 29, 2024
+ *      Author: Jan Mareš
+ */
+
+
---
+++ STM32G0_USBPD_Sink_1.2/Application/app.h
@@ -0,0 +1,13 @@
+/*
+ * app.h
+ *
+ *  Created on: Apr 29, 2024
+ *      Author: Jan Mareš
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+
+
+#endif /* APP_H_ */
